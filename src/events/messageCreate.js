30c0994805e4770873e08c3f4f1f32027f1f88b6
Andres Copeticona <andrescopeticona@outlook.es>
2021-10-23 17:49:37 -0400

    arreglado problema con intents de audio

--- src/events/messageCreate.js
+++ src/events/messageCreate.js
@@ -1,8 +1,24 @@
+const { join } = require('path');
 const { prefix } = require('../../config.json');
+const Gtts = require('gtts');
+const {
+	joinVoiceChannel,
+	createAudioPlayer,
+	createAudioResource,
+	NoSubscriberBehavior,
+	generateDependencyReport,
+} = require('@discordjs/voice');
+
 module.exports = {
 	name: 'messageCreate',
 	run(client, message) {
 		if (message.author.bot) return;
+		if (message.content.startsWith('.')) {
+			if (!message.member.voice.channel)
+				return message.channel.send('Ingresa a un canal de voz');
+			tts(message);
+			return;
+		}
 		if (!message.content.startsWith(prefix)) return;
 		let [cmdname, ...cmdargs] = message.content
 			.slice(prefix.length)
@@ -14,4 +30,24 @@ module.exports = {
 		if (!cmd) return;
 		cmd.run(client, message, cmdargs);
 	},
+};
+
+const tts = (msg) => {
+	let gtts = new Gtts(msg.content, 'es');
+	const file = join(__dirname, '../../tts.mp3');
+	const channel = msg.member.voice.channel;
+	const player = createAudioPlayer();
+
+	const connection = joinVoiceChannel({
+		channelId: channel.id,
+		guildId: msg.guild.id,
+		adapterCreator: msg.guild.voiceAdapterCreator,
+	});
+
+	gtts.save(file, (err, result) => {
+		if (err) throw new Error(err);
+		const resource = createAudioResource(file);
+		player.play(resource);
+		connection.subscribe(player);
+	});
 };
--- src/index.js
+++ src/index.js
@@ -3,7 +3,11 @@ const path = require('path');
 const { DISCORD_TOKEN: token } = require('../config.json');
 const { Client, Intents, Collection } = require('discord.js');
 const client = new Client({
-	intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
+	intents: [
+		Intents.FLAGS.GUILDS,
+		Intents.FLAGS.GUILD_MESSAGES,
+		Intents.FLAGS.GUILD_VOICE_STATES,
+	],
 });
 
 client.commands = new Collection();
