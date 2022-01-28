1a2282e19e27c595c58074abe0d739eca262bebe
Andres Copeticona <andrescopeticona@outlook.es>
2022-01-28 10:53:18 -0400

    change random commando to TS

--- src/commands/random.ts
+++ src/commands/random.ts
@@ -1,26 +1,25 @@
-// module.exports = {
-// 	name: 'random',
-// 	desc: 'Selecciona una opción random',
-// 	usage: '',
-// 	aliases: ['r'],
-// 	isPrivate: false,
-// 	guildOnly: false,
-// 	category: 'utilitis',
-// 	isOwner: true,
-// 	run: async (client, message, args) => {
-// 		let random = Math.floor(Math.random() * args.length);
-// 		if (args.length === 0)
-// 			return message.channel.send('Debes ingresar al menos una opción');
+import { ICommand } from '../Interfaces/ICommand';
 
-// 		message.channel.send({
-// 			embeds: [
-// 				{
-// 					title: 'Ganador',
-// 					type: 'rich',
-// 					description: args[random],
-// 					color: 10181046,
-// 				},
-// 			],
-// 		});
-// 	},
-// };
+const command: ICommand = {
+	name: 'random',
+	description: 'Generate a random Winner from a list of options',
+	aliases: ['r'],
+	run: async (client, message, args) => {
+		let random = Math.floor(Math.random() * args.length);
+		if (args.length === 0)
+			return message.channel.send('Debes ingresar al menos una opción');
+
+		message.channel.send({
+			embeds: [
+				{
+					title: 'Ganador',
+					type: 'rich',
+					description: args[random],
+					color: 10181046,
+				},
+			],
+		});
+	},
+};
+
+export default command;
